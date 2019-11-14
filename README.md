55de23fb15ecc5e324a7f573fd36095a7dac8aa3
zk <271226192@qq.com>
2019-11-14 15:22:59 +0800

    完成RandomUtils测试类的编写, 修改README.

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 一个封装了Java Web开发常用功能的库.(JDK要求至少1.8及以上)
 
 本仓库将持续更新!
+
+**1.`top.jasonkayzk.jutil.RandomUtils`类**
+|方法名|简要说明|
+|------|---------------|
+|getRandomInteger|按照length, 或者range获取一个随机的整型|
+|getRandomIntegerIgnoreRange|获取一个可以忽略某个区间的随机整型|
+|getRandomString|获取一个随机字符串|
+|getRandomText|安装给定ascii范围, 获取一个随机字符串|
+|getRandomTextIgnoreRange|获取一个可以忽略某个范围字符的随机字符串|
+|getRandomStringWithoutSymbol|获取一个只有数字和字母的随机字符串|
+|getRandomStringOnlyLetter|获取一个只有字母的随机字符串|
+|getRandomStringOnlyLowerCase|获取一个只有小写字母的随机字符串|
+|getRandomStringOnlyUpperCase|获取一个只有大写字母的随机字符串|
+|getRandomDouble|获取一个随机的双精度类型|
+|getRandomNumber|获取一个指定长度的随机数字字符串|
+|getRandomColor|获取一个指定透明度的随机颜色(R,G,B)|
+
+
--- src/test/java/top/jasonkayzk/jutil/RandomUtilsTest.java
+++ src/test/java/top/jasonkayzk/jutil/RandomUtilsTest.java
@@ -9,6 +9,13 @@ import top.jasonkayzk.jutil.RandomUtils;
 public class RandomUtilsTest {
 
     @Test
+    public void testGetRandomInteger() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtils.getRandomInteger());
+        }
+    }
+
+    @Test
     public void testRandomIntegerByRange() {
         for (int i = 0; i < 100; ++i) {
             System.out.println(RandomUtils.getRandomInteger(0, 1000));
@@ -43,6 +50,81 @@ public class RandomUtilsTest {
         }
     }
 
+    @Test
+    public void getRandomStringWithoutSymbol() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtils.getRandomStringWithoutSymbol(i));
+        }
 
+    }
 
+    @Test
+    public void getRandomTextIgnoreRange() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtils.getRandomTextIgnoreRange(32, 122, i, new int[] {32, 44}));
+        }
+    }
+
+    @Test
+    public void getRandomStringOnlyLetter() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtils.getRandomStringOnlyLetter(i));
+        }
+    }
+
+    @Test
+    public void getRandomStringOnlyLowerCase() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtils.getRandomStringOnlyLowerCase(i));
+        }
+    }
+
+    @Test
+    public void getRandomStringOnlyUpperCase() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtils.getRandomStringOnlyUpperCase(i));
+        }
+    }
+
+    @Test
+    public void getRandomNumber() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtils.getRandomNumber(i));
+        }
+    }
+
+    @Test
+    public void getRandomDouble() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtils.getRandomDouble());
+        }
+    }
+
+    @Test
+    public void testGetRandomDouble() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtils.getRandomDouble(-5.0, 10.5));
+        }
+    }
+
+    @Test
+    public void testGetRandomDouble1() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtils.getRandomDouble(-5.0, 10.5, 4));
+        }
+    }
+
+    @Test
+    public void getRandomColor() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtils.getRandomColor(0.5));
+        }
+    }
+
+    @Test
+    public void testGetRandomColor() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtils.getRandomColor());
+        }
+    }
 }
